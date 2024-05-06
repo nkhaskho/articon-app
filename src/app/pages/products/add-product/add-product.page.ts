003471418e2e514a4febbacfafa109be6fd95fbc
Naiim Khaskhoussi <nkhaskho@gmail.com>
2024-05-06 14:20:24 +0100

    Fetching categories
    Signed-off-by: Naiim Khaskhoussi <nkhaskho@gmail.com>

--- src/app/pages/products/add-product/add-product.page.ts
+++ src/app/pages/products/add-product/add-product.page.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonCardSubtitle, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonList, IonLabel, IonInput, IonItem, IonButton } from '@ionic/angular/standalone';
 import { Product } from 'src/app/models/product';
+import { ProductService } from 'src/app/services/products/product.service';
+import { ProductCategory } from 'src/app/models/product-category';
 
 @Component({
   selector: 'app-add-product',
@@ -14,11 +16,14 @@ import { Product } from 'src/app/models/product';
 export class AddProductPage implements OnInit {
 
   product: Product = new Product();
+  categories: ProductCategory[] = [];
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
-    return
+    this.productService.getCategories().toPromise()
+    .then(res => console.log)
+    .catch(err => console.log)
   }
 
   add() {
--- src/app/services/products/product.service.ts
+++ src/app/services/products/product.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from 'src/app/models/product';
+import { ProductCategory } from 'src/app/models/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  endpoint = "http://localhost:8000/api/products";
+
+  constructor(private http: HttpClient) { }
+
+  getCategories() {
+    let headers = new HttpHeaders().set('Access-Control-Allow-Origin',"http://localhost:4200/");
+    return this.http.get<ProductCategory[]>(this.endpoint+"/categories", {headers: headers});
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.endpoint);
+  }
+
 }
