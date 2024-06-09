4cf576038594a5c64db39e2267dd7c3cdfa3c851
Naiim Khaskhoussi <nkhaskho@gmail.com>
2024-06-10 00:37:11 +0100

    Fix home page content
    Signed-off-by: Naiim Khaskhoussi <nkhaskho@gmail.com>

--- src/app/pages/home/home/home.page.ts
+++ src/app/pages/home/home/home.page.ts
@@ -1,23 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonItem } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonItem, IonCardSubtitle, IonCardContent, IonCardTitle, IonCardHeader, IonCard } from '@ionic/angular/standalone';
+import { ProductService } from 'src/app/services/products/product.service';
+import { EventService } from 'src/app/services/events/event.service';
+import { Product } from 'src/app/models/product';
+import { Event } from 'src/app/models/event';
+import { User } from 'src/app/models/user';
+import { UserService } from 'src/app/services/users/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonItem, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle, IonItem, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class HomePage implements OnInit {
 
   appStorage = localStorage;
+  products: Product[] = [];
+  events: Event[] = [];
+  users: User[] = [];
   
-  constructor() { }
+  constructor(private productService: ProductService,
+              private eventsService: EventService,
+              private userService: UserService,
+              // TODO
+  ) { }
 
   ngOnInit() {
-    console.log()
+    this.productService.getProducts("").subscribe(
+      data => this.products = data,
+      err => console.log
+    )
+    this.eventsService.getEvents("").subscribe(
+      data => this.events = data,
+      err => console.log
+    )
+    this.userService.findAll().subscribe(
+      data => this.users = data,
+      err => console.log
+    )
   }
 
 }
