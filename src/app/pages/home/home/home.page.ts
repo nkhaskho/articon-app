aa0516c7d0984b9283630f0a0d410f644e1c53cf
Naiim Khaskhoussi <nkhaskho@gmail.com>
2024-06-09 22:23:53 +0100

    Add home page message
    Signed-off-by: Naiim Khaskhoussi <nkhaskho@gmail.com>

--- src/app/pages/home/home/home.page.ts
+++ src/app/pages/home/home/home.page.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonItem } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonItem, IonLabel, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class HomePage implements OnInit {
 
+  appStorage = localStorage;
+  
   constructor() { }
 
   ngOnInit() {
+    console.log()
   }
 
 }
