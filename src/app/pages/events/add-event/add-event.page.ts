82e925a48e269b78201f0825d0a97af418907c38
Naiim Khaskhoussi <nkhaskho@gmail.com>
2024-06-09 22:36:19 +0100

    Fix events page
    Signed-off-by: Naiim Khaskhoussi <nkhaskho@gmail.com>

--- src/app/pages/events/add-event/add-event.page.ts
+++ src/app/pages/events/add-event/add-event.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonSelect, IonSelectOption, IonContent, IonHeader, IonTitle, IonToolbar, IonCardSubtitle, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonList, IonLabel, IonInput, IonItem, IonButton } from '@ionic/angular/standalone';
+import { IonSelect, IonSelectOption, IonContent, IonHeader, IonTitle, IonToolbar, IonCardSubtitle, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonList, IonLabel, IonInput, IonItem, IonButton, IonTextarea } from '@ionic/angular/standalone';
 import { Event } from 'src/app/models/event';
 import { EventService } from 'src/app/services/events/event.service';
 import { Router } from '@angular/router';
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
   templateUrl: './add-event.page.html',
   styleUrls: ['./add-event.page.scss'],
   standalone: true,
-  imports: [IonSelect, IonSelectOption, IonButton, IonItem, IonInput, IonLabel, IonList, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonCardSubtitle, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonTextarea, IonSelect, IonSelectOption, IonButton, IonItem, IonInput, IonLabel, IonList, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonCardSubtitle, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 
 })
 export class AddEventPage implements OnInit {
